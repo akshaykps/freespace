05ccee164afb9116cc99b3a2b49f0ab0836d5fb6
AkshayKPS <akshaykapse2194@gmail.com>
2024-12-03 18:02:09 +0530

    Project setup Done

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className={"flex-center h-screenc"}>
+        Free Space - Store your digital belongings on your own free space
+      </h1>
+    </div>
+  );
+}
