b24602cc26835b7eeaca1b2c5100e396dd42f0ed
AkshayKPS <akshaykapse2194@gmail.com>
2024-12-03 23:14:24 +0530

    Auth Layout and AuthForm Done

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AuthFormComponent from "@/components/AuthForm";
+
+const SignInPage = () => {
+    return (
+        <AuthFormComponent type={"sign-in"} />
+    )
+}
+export default SignInPage
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AuthFormComponent from "@/components/AuthForm";
+
+const SignUpPage = () => {
+    return (
+        <AuthFormComponent type={"sign-up"} />
+    )
+}
+export default SignUpPage
